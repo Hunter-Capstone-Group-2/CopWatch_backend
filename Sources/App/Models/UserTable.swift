9ef98e7af34f2dcc8c14e957f40a52a61ad5501c
Raul Camargo <Raul.Camargo89@myhunter.cuny.edu>
2023-03-13 04:19:27 -0400

    Not sure what I did

--- Sources/App/Models/UserTable.swift
+++ Sources/App/Models/UserTable.swift
@@ -9,17 +9,15 @@ import Vapor
 import Fluent
 
 final class User: Model {
+    typealias IDValue = <#type#>
+    
     
     // Table name
     static let schema = "user"
     
-    
-    @ID(key: .id)
-    var id: UUID?
-    
     // Table columns
-    @Field(key: "user_id")
-    var user_id: String
+    @ID(custom: "user_id", generatedBy: .user)
+    var user_id: String?
     
     @Field(key: "user_name")
     var user_name: String
@@ -27,18 +25,17 @@ final class User: Model {
     @Field(key: "location_id")
     var location_id: String
     
-    @Field(key: "pins_created")
-    var pins_created: Int
+    @OptionalField(key: "pins_created")
+    var pins_created : Int?
     
-    @Field(key: "pins_confirmed")
-    var pins_confirmed: Int
+    @OptionalField(key: "pins_confirmed")
+    var pins_confirmed: Int?
     
     init() { }
     
     init(id: UUID? = nil, user_id: String, user_name: String,
          location_id: String, pins_created: Int, pins_confirmed: Int )
         {
-            self.id = id
             self.user_id = user_id
             self.user_name = user_name
             self.location_id = location_id
