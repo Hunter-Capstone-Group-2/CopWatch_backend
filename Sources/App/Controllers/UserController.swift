bd80cf2318663f94149672d90254e40b7edf3bb9
Raul Camargo <Raul.Camargo89@myhunter.cuny.edu>
2023-03-17 12:17:10 -0400

    POST and GET request working for user table. Could not get CodingKeys enum to work but it is not necessary.

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -13,10 +13,17 @@ struct UserController: RouteCollection
     func boot(routes: Vapor.RoutesBuilder) throws {
         let user = routes.grouped("user")
         user.get(use: index)
+        user.post(use: create)
     }
     func index(req: Request) throws -> EventLoopFuture<[User]>
     {
         return User.query(on: req.db).all()
+    }
+    
+    func create(req: Request) throws -> EventLoopFuture<HTTPStatus>
+    {
+        let user = try req.content.decode(User.self)
+        return user.save(on: req.db).transform(to: .ok)
     }
     
 }
