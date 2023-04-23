2c6d620c8b1475ba5a7e0bce84d78518976e0a45
Raul Camargo <Raul.Camargo89@myhunter.cuny.edu>
2023-04-23 03:30:47 -0400

    GET request based on userID will return that users comments.

--- Sources/App/Controllers/CommentController.swift
+++ Sources/App/Controllers/CommentController.swift
@@ -14,13 +14,16 @@ struct CommentTableController: RouteCollection
         let comment = routes.grouped("comment")
         comment.get(use: index)
         comment.post(use: create)
+        comment.get(":userID", use: getUserPosts)
     }
    
+    // GET // Returns ALL comments in table. BaseURL/comment
     func index(req: Request) async throws -> [Comment]
     {
         try await Comment.query(on: req.db).all()
     }
     
+    // POST // Creates new comment. BaseURL/comment
     func create(req: Request) async throws -> HTTPStatus
     {
         let newComment = try req.content.decode(CommentTablePatch.self)
@@ -31,4 +34,15 @@ struct CommentTableController: RouteCollection
         try await comment.save(on: req.db)
         return .ok
     }
+    
+    // GET // Returns specific user comments. BaseURL/comment/userID
+    func getUserPosts(req: Request) async throws -> [Comment]
+    {
+        let identifier = req.parameters.get("userID")!
+        
+        return try await Comment.query(on: req.db)
+                .filter(\.$userID == identifier)
+                .all()
+    }
+    
 }
