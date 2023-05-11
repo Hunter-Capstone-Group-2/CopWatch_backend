ed9d5ae20fa805d88f2965d6a00fe854ac4fa64a
Raul Camargo <Raul.Camargo89@myhunter.cuny.edu>
2023-05-11 02:06:44 -0400

    Updated packages and replaced deprecated method in configure function

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,12 +8,12 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    app.databases.use(.postgres(configuration: .init(hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                                                     username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+                                                     password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+                                                     database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+                                                     tls: .prefer(try .init(configuration: .clientDefault))
+                                                    )
     ), as: .psql)
 
     app.migrations.add(EnablePostGISMigration())
